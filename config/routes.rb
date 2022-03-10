315efadf5b14224081e7c1150a74e9c3d4bd39aa
Kyle Rego <regoky@outlook.com>
2022-03-10 15:55:33 -0500

    Add sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
 
+  get '/signup', to: 'users#new'
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
