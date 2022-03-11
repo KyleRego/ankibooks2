4021c5cbb2e72b560c4097e751d17eafcd185f30
Kyle Rego <regoky@outlook.com>
2022-03-11 15:13:29 -0500

    Work on logging in users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+
   get '/', to: 'static_pages#welcome'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
 
   get '/signup', to: 'users#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 
