fec465bbdbcc579ce20d17e2e03c3fe0020f3b40
Kyle Rego <regoky@outlook.com>
2022-03-11 16:17:18 -0500

    Add logging out

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,22 +6,30 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:kyle)
   end
   
-  test "login with valid information" do
+  test "login with valid information followed by logout" do
     get login_path
     post login_path, params: { session: { email: @user.email,
                                           password: 'password' } }
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to '/'
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
   end
 
   test "login with valid email/invalid password" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, params: { session: { email: 'example@outlook.com',
+    post login_path, params: { session: { email: @user.email,
                                           password: 'aaa' } }
+    assert_not is_logged_in?
     assert_template 'sessions/new'
     assert_not flash.empty?
     get '/'
