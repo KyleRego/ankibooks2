b9bb868a7626e7ca5a21b7ac40d3c870f1cf6758
Kyle Rego <regoky@outlook.com>
2022-03-12 15:52:07 -0500

    Work on adding new articles

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,5 +1,10 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-
+  test 'should get new' do
+    log_in_for_test
+    @book = books(:one)
+    get new_book_article_path(@book.id)
+    assert_response :success
+  end
 end
